06040f4f81db87e75e7e6f96ecf9df0f311d6dcd
Illya Gerasymchuk <illya@iluxonchik.me>
2023-11-05 06:44:40 +0000

    refactor: separate smart contracts among multiple modules

---
+++ contracts/DTO.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.20;
+
+struct CollateralToken {
+    address token;
+    uint256 amount;
+}
+
--- contracts/DeFiDeTo.sol
+++ contracts/DeFiDeTo.sol
@@ -6,13 +6,12 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "./DTO.sol";
 
-
-struct CollateralToken {
-    address token;
-    uint256 amount;
-}
-
+/*
+    DeFiDeTo is a token that is backed by a basket of other tokens.  The value of DeFiDeTo is pegged to the value of the
+    basket of tokens.
+*/
 contract DeFiDeTo is ERC20Burnable, Ownable {
     using SafeERC20 for IERC20;
 
@@ -85,37 +84,3 @@ contract DeFiDeTo is ERC20Burnable, Ownable {
     }
 }
 
-contract DeFiDe {
-    using SafeMath for uint256;
-    using SafeERC20 for IERC20;
-
-    mapping(uint256 => address) public deployedDeFiDeToInstance;
-
-    function getOrCreateDerivativeTokenForTokenAddressesAndProportions(CollateralToken[] calldata tokens) external returns (address) {
-        // Calculate token proportions and create a hash index
-        uint256 index = computeIndexForUnderlyingTokens(tokens);
-
-        // Check if a DeFiDeTo instance already exists, if not create one
-        if (deployedDeFiDeToInstance[index] == address(0)) {
-            deployedDeFiDeToInstance[index] = createDerivativeTokenForTokenAddressesAndProportions(tokens);
-        }
-        return deployedDeFiDeToInstance[index];
-    }
-
-    function computeIndexForUnderlyingTokens(CollateralToken[] memory tokens) public pure returns (uint256) {
-        bytes memory packedValues;
-        for (uint256 i = 0; i < tokens.length; i++) {
-            packedValues = abi.encodePacked(packedValues, tokens[i].token);
-            packedValues = abi.encodePacked(packedValues, tokens[i].amount);
-        }
-        bytes32 hash = keccak256(packedValues);
-        return uint256(hash);
-    }
-
-    function createDerivativeTokenForTokenAddressesAndProportions(CollateralToken[] memory tokens) internal returns (address) {
-        DeFiDeTo deFiDeToInstance = new DeFiDeTo(tokens);
-        uint256 index = computeIndexForUnderlyingTokens(tokens);
-        deployedDeFiDeToInstance[index] = address(deFiDeToInstance);
-        return address(deFiDeToInstance);
-    }
-}
